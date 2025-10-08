ad51cc82feebd28665eee3b0453d2d23c1076001
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-10-08 20:39:43 -0300

    fix: temporariamente o userId fixo como "1" no PostForm.tsx
     - Schema em prisma/schema.prisma atualizado para definir userId padrão como 1

---
+++ prisma/migrations/20251008232609_add_default_userid/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL DEFAULT 1,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Post" ("body", "createdAt", "id", "title", "updatedAt", "userId") SELECT "body", "createdAt", "id", "title", "updatedAt", "userId" FROM "Post";
+DROP TABLE "Post";
+ALTER TABLE "new_Post" RENAME TO "Post";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation"; // Importando useRouter para navega
 // Componente de formulário para criar um novo post
 
 export default function PostForm() {
-  const [userId, setUserId] = useState("");
+  const [userId, setUserId] = useState("1"); // Temporariamente fixo como "1"
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [submitting, setSubmitting] = useState(false);
@@ -15,7 +15,7 @@ export default function PostForm() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!userId || !title || !body) {
+    if (!title || !body) {
       alert("Todos os campos são obrigatórios.");
       return;
     }
@@ -29,6 +29,7 @@ export default function PostForm() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
+          // O userId sera sempre 1 temporariamente
           userId: Number(userId),
           title,
           body,
@@ -39,7 +40,7 @@ export default function PostForm() {
       }
 
       // Limpa os campos do formulário
-      setUserId("");
+      // setUserId(""); removido temporariamente
       setTitle("");
       setBody("");
       router.refresh(); // Atualiza a página para mostrar o novo post
@@ -55,16 +56,16 @@ export default function PostForm() {
       <div className="flex flex-col items-center gap-5 justify-center md:gap-18 md:flex-row">
         <input
           type="number"
-          name={userId}
+          value={userId}
           onChange={(e) => setUserId(e.target.value)}
           id="userId"
-          className="bg-white rounded-md w-80 h-12 outline-main-orange-color pl-6"
+          className="bg-gray-200 cursor-not-allowed rounded-md w-80 h-12 outline-main-orange-color pl-6"
           placeholder="ID do Usuário"
-          required
+          disabled // Temporariamente desabilitado
         />
         <input
           type="text"
-          name={title}
+          value={title}
           onChange={(e) => setTitle(e.target.value)}
           id="title"
           className="bg-white rounded-md w-80 h-12 outline-main-orange-color pl-6"
@@ -72,7 +73,7 @@ export default function PostForm() {
           required
         />
         <textarea
-          name={body}
+          value={body}
           onChange={(e) => setBody(e.target.value)}
           id="body"
           className="bg-white rounded-md w-80 h-12 outline-main-orange-color pl-6 resize-none pt-3"
