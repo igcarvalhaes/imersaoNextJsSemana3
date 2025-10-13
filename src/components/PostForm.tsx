3df64ba77ae5264de71aa8d0a450ae4d5c33849d
igcarvalhaes <igcarvalhaes@hotmail.com>
2025-10-13 11:53:45 -0300

    fix: reset bugs

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation"; // Importando useRouter para navega
 // Componente de formulário para criar um novo post
 
 export default function PostForm() {
-  const [userId, setUserId] = useState("1"); // Temporariamente fixo como "1"
+  //const [userId, setUserId] = useState("1");  Temporariamente fixo como "1"
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [submitting, setSubmitting] = useState(false);
@@ -29,8 +29,7 @@ export default function PostForm() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          // O userId sera sempre 1 temporariamente
-          userId: Number(userId),
+          userId: 1,
           title,
           body,
         }),
@@ -53,16 +52,7 @@ export default function PostForm() {
   };
   return (
     <form onSubmit={handleSubmit} className="w-full flex flex-col gap-10 pt-20">
-      <div className="flex flex-col items-center gap-5 justify-center md:gap-18 md:flex-row">
-        <input
-          type="number"
-          value={userId}
-          onChange={(e) => setUserId(e.target.value)}
-          id="userId"
-          className="bg-gray-200 cursor-not-allowed rounded-md w-80 h-12 outline-main-orange-color pl-6"
-          placeholder="ID do Usuário"
-          disabled // Temporariamente desabilitado
-        />
+      <div className="flex flex-col items-center gap-5 justify-center  md:gap-28 md:justify-center md:flex-row">
         <input
           type="text"
           value={title}
@@ -79,14 +69,14 @@ export default function PostForm() {
           className="bg-white rounded-md w-80 h-12 outline-main-orange-color pl-6 resize-none pt-3"
           placeholder="Conteúdo do post"
         />
+        <button
+          type="submit"
+          disabled={submitting}
+          className="h-12 w-80 md:w-60 bg-main-orange-color text-white rounded-md hover:bg-orange-600 transition-colors hover:cursor-pointer"
+        >
+          {submitting ? "Postando..." : "Postar"}
+        </button>
       </div>
-      <button
-        type="submit"
-        disabled={submitting}
-        className="h-12 w-60 bg-main-orange-color text-white rounded-md mx-auto hover:bg-orange-600 transition-colors hover:cursor-pointer"
-      >
-        {submitting ? "Postando..." : "Postar"}
-      </button>
     </form>
   );
 }
